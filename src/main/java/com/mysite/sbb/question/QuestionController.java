73ee348aa03d1233db4d25f4e480b65c8f89eb72
wnstn7671 <wnstn7671@naver.com>
2023-06-20 16:11:59 +0900

    Feat: getQuestion 메소드 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -23,8 +23,9 @@ public class QuestionController {
 
     @GetMapping("/question/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
-        model.addAttribute("id", id);
+        Question question = questionService.getQuestion(id);
 
+        model.addAttribute("question", question);
         return "question_detail";
     }
 }
