f29a9b3a5f117b773d2a800bc2da8a2b4f555e28
wnstn7671 <wnstn7671@naver.com>
2023-06-20 16:05:51 +0900

    Feat: 질문 상세 컨트롤러 , detail 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,5 +19,12 @@ public class QuestionController {
 
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        model.addAttribute("id", id);
+
+        return "question_detail";
     }
 }
