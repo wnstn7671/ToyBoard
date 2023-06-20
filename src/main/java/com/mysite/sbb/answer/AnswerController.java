8eccedef7b1c949a0242ccf980a6a6592fb8461e
wnstn7671 <wnstn7671@naver.com>
2023-06-20 16:16:50 +0900

    Feat: 답변 컨트롤러 추가

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+public class AnswerController {
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable Integer id, @RequestParam String content) {
+        // 관련 질문을 얻어온다.
+        Question question = questionService.getQuestion(id);
+
+        // TODO : 답변객체를 만든다.
+
+        return "redirect:/question/detail/%d".formatted(id);
+    }
+}
